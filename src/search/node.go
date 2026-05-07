ced257855a13c1ba0fa5c099d7ea4eb2e35026b8
NazwanSM <nazwan.siddqi@gmail.com>
2026-05-07 17:54:13 +0700

    feat: add Node struct and Path reconstruction

---
+++ src/search/node.go
@@ -0,0 +1,29 @@
+package search
+
+import (
+	"iceslide/src/state"
+	"iceslide/src/successor"
+)
+
+type Node struct {
+	State  state.State
+	Parent *Node
+	Action successor.Direction
+	G      int
+	H      int
+}
+
+func (n *Node) F() int { return n.G + n.H }
+
+func (n *Node) Path() []*Node {
+	depth := 0
+	for cur := n; cur != nil; cur = cur.Parent {
+		depth++
+	}
+	path := make([]*Node, depth)
+	for cur := n; cur != nil; cur = cur.Parent {
+		depth--
+		path[depth] = cur
+	}
+	return path
+}
